cafdc1c3c43a54633d8eb04a70f71bde68582588
zifeng <zifeng.1024@qq.com>
2020-08-20 11:23:14 +0800

    envoy wasm filter.

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,38 @@
-use proxy_wasm::traits::{Context, HttpContext};
-use proxy_wasm::types::Action;
+use log::trace;
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+use primes::is_prime;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[no_mangle]
+pub fn _start() {
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_http_context(|context_id, _| -> Box<dyn HttpContext> {
+        Box::new(PrimeAuthorizer { context_id })
+    });
 }
 
-struct JwtAuthorizer {
-    context_id: i32,
+struct PrimeAuthorizer {
+    context_id: u32,
 }
 
-impl Context for JwtAuthorizer {}
+impl Context for PrimeAuthorizer {}
 
-impl HttpContext for JwtAuthorizer {
+impl HttpContext for PrimeAuthorizer {
     fn on_http_request_headers(&mut self, _: usize) -> Action {
         for (name, value) in &self.get_http_request_headers() {
-            println!("In WASM : #{} -> {}: {}", self.context_id, name, value);
+            trace!("In WASM : #{} -> {}: {}", self.context_id, name, value);
         }
-        let token = &String::from("jwt_token");
-        match self.get_http_request_header("Authorization") {
-            Some(Authorization) if Authorization.eq(token) => {
+
+        match self.get_http_request_header("token") {
+            Some(token) if token.parse::<u64>().is_ok() && is_prime(token.parse().unwrap()) => {
                 self.resume_http_request();
                 Action::Continue
             }
             _ => {
                 self.send_http_response(
                     403,
-                    vec![("Powered By", "proxy-wasm")],
-                    Some(b"Access Forbidden.\n"),
+                    vec![("Powered-By", "proxy-wasm")],
+                    Some(b"Access forbidden.\n"),
                 );
                 Action::Pause
             }
